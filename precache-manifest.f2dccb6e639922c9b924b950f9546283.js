51e8a2ab06c47b05a74c70c2ffc28a5e05ee370c
devanshgl66 <devanshgl55@gmail.com>
2020-06-14 00:18:30 +0530

    Updates

---
+++ precache-manifest.f2dccb6e639922c9b924b950f9546283.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796b69464169cfcb15eec42753ba373c",
+    "url": "/AdoptMe/index.html"
+  },
+  {
+    "revision": "47f4f5a4dabbf94c90d9",
+    "url": "/AdoptMe/static/css/2.939c0d2d.chunk.css"
+  },
+  {
+    "revision": "b8e34f2f085d592e6938",
+    "url": "/AdoptMe/static/css/main.e7fbcf21.chunk.css"
+  },
+  {
+    "revision": "47f4f5a4dabbf94c90d9",
+    "url": "/AdoptMe/static/js/2.9b9e4d94.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/AdoptMe/static/js/2.9b9e4d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e34f2f085d592e6938",
+    "url": "/AdoptMe/static/js/main.f1d7523e.chunk.js"
+  },
+  {
+    "revision": "d09dac360916d906c48c",
+    "url": "/AdoptMe/static/js/runtime-main.6184fce1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/AdoptMe/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/AdoptMe/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/AdoptMe/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/AdoptMe/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/AdoptMe/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
